e775386bf70ffa5cf0a566f972f2f5c269cff90d
Diego Rodriguez <20230602@ricaldone.edu.sv>
2024-05-21 15:13:03 -0600

    Agregue el boton de Eliminar mascotas

--- app/src/main/java/RecycleViewHelper/Adaptador.kt
+++ app/src/main/java/RecycleViewHelper/Adaptador.kt
@@ -1,16 +1,19 @@
 package RecyclerViewHelper
 
 import Daniel.Soriano.crud_daniel_soriano.R
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import modelo.claseConexion
 import modelo.dataClassMascotas
+import java.sql.PreparedStatement
 
 
 class Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapter<ViewHolder>() {
@@ -20,6 +23,28 @@ class Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapt
         //esto es lo que notifica al recycleView que hay nuevos datos agregados a la base de datos
         notifyDataSetChanged()
     }
+    fun EliminarDatos(nombreMascota: String, posicion: Int){
+        val listaDatos = Datos.toMutableList()
+        listaDatos.removeAt(posicion)
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val objConnection = claseConexion().cadenaConexion()
+
+            //2- Crear una variable que contenga un PrepareStatement
+            val deleteMascotas = objConnection?.prepareStatement("Delete From tbMascota where nombreMascota = ?")!!
+            deleteMascotas.setString(1, nombreMascota)
+            deleteMascotas.executeUpdate()
+
+            val commit = objConnection.prepareStatement("commit")!!
+            commit.executeUpdate()
+
+
+        }
+        Datos = listaDatos.toList()
+        notifyItemRemoved(posicion)
+        notifyDataSetChanged()
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val vista =
@@ -32,6 +57,24 @@ class Adaptador(private var Datos: List<dataClassMascotas>) : RecyclerView.Adapt
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val producto = Datos[position]
         holder.textView.text = producto.nombreMascota
+
+        holder.btnEliminar.setOnClickListener {
+            val context = holder.itemView.context
+            val Builder = AlertDialog.Builder(context)
+
+            Builder.setTitle("Eliminar")
+            Builder.setMessage("¿Desea eliminar la mascota?")
+
+            //botones
+
+            Builder.setPositiveButton("si"){dialog, switch -> EliminarDatos(producto.nombreMascota, position)}
+
+            Builder.setNegativeButton("No"){dialog, switch -> dialog.dismiss()}
+
+
+            val dialog = Builder.create()
+            dialog.show()
+        }
     }
 
 
--- app/src/main/java/RecycleViewHelper/ViewHolder.kt
+++ app/src/main/java/RecycleViewHelper/ViewHolder.kt
@@ -2,6 +2,7 @@ package RecyclerViewHelper
 
 import Daniel.Soriano.crud_daniel_soriano.R
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -9,4 +10,6 @@ import androidx.recyclerview.widget.RecyclerView
 
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val textView: TextView = view.findViewById(R.id.txtMascotaCard)
+    val btnEditar: ImageView = view.findViewById(R.id.btnEditar)
+    val btnEliminar: ImageView = view.findViewById(R.id.btnEliminar)
 }
